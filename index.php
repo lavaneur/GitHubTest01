68b2e9bcd629e1331f80c4299ca8a7999768fcf7
Lavaneur <gaurav81083@gmail.com>
2019-03-28 14:33:20 +0800

    Saving changes v1.2

--- index.php
+++ index.php
@@ -1,7 +1,19 @@
 <?php
 
+# Changes v1.0
 echo "This is first assignment for GIT"
 echo "Updated the file to save changes in GIT"
 echo "Adding new line in file"
+
+# Changes v1.1
+echo "This is first assignment for GIT"
+echo "Updated the file to save changes in GIT"
+echo "Adding new line in file"
+
+# Changes v1.2
+echo "This is first assignment for GIT"
+echo "Updated the file to save changes in GIT"
+echo "Adding new line in file"
+
 
 ?>
